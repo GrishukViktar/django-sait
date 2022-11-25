8e77f9d4b7338809b40c262a04760deb080bf220
Виктор Грищук <100824226+GrishukViktar@users.noreply.github.com>
2022-11-25 07:30:38 +0300

    second commiet,add mysite,newdjango

---
+++ mysite/main/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return render(request, 'main/index.html')
+# def index(request):
+#     return render(request, 'main/templates/main/index.html')
+
+
+def about(request):
+    return render(request, 'templates/main/about.html')
+
